3745dec345c8da9b6c3ec779fc0729654fb37af2
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-05-28 21:24:00 +0300

    added redux

--- hw-rn-app/App.js
+++ hw-rn-app/App.js
@@ -1,7 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { useRoute } from "./router";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 export default App = () => {
   const routing = useRoute();
-  return <NavigationContainer>{routing}</NavigationContainer>;
+  return (
+    <Provider store={store}>
+      <NavigationContainer>{routing}</NavigationContainer>
+    </Provider>
+  );
 };
